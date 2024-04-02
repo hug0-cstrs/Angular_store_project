a8a6febd6605922263d89c9a78f801b2ea2c8ddf
hug0-cstrs <hugo.casteras@outlook.fr>
2024-04-02 23:31:00 +0000

    store_project_file

---
+++ src/app/pages/home/components/filters/filters.component.ts
@@ -0,0 +1,40 @@
+import {
+  Component,
+  EventEmitter,
+  OnDestroy,
+  OnInit,
+  Output,
+} from "@angular/core";
+import { StoreService } from "../../../../services/store.service";
+import { Subscription } from "rxjs";
+
+@Component({
+  selector: "app-filters",
+  templateUrl: "./filters.component.html",
+})
+export class FiltersComponent implements OnInit, OnDestroy {
+  @Output() showCategory = new EventEmitter<string>();
+  categoriesSubscription: Subscription | undefined;
+
+  categories: Array<string> | undefined;
+
+  constructor(private storeService: StoreService) {}
+
+  ngOnInit(): void {
+    this.categoriesSubscription = this.storeService
+      .getAllCategories()
+      .subscribe((respondse) => {
+        this.categories = respondse;
+      });
+  }
+
+  onShowCategory(category: string): void {
+    this.showCategory.emit(category);
+  }
+
+  ngOnDestroy(): void {
+    if (this.categoriesSubscription) {
+      this.categoriesSubscription.unsubscribe();
+    }
+  }
+}
